f2b178870903b7f2b2545f765ba783847b59b752
GeekTaj <40384195+GeekTaj@users.noreply.github.com>
2018-07-13 17:42:02 +0800

    Add files via upload

---
+++ chat_server.js
@@ -0,0 +1,120 @@
+var socketio = require('socket.io');
+var io;
+var guestNumber = 1;
+var nickNames = {};
+var namesUsed = [];
+var currentRoom = {};
+
+exports.listen = function(server){
+	io = socketio.listen(server);
+	io.set('log level',1);
+	console.log(server);
+	console.log('chat_server.js connected!')
+	io.sockets.on('connection', function(socket){
+		console.log("one guest connected!");
+		guestNumber = assignGuestName(socket,guestNumber,nickNames,namesUsed);
+		joinRoom(socket,'Lobby');
+		handleMessageBroadCasting(socket,nickNames);
+		handleNameChangeAttemps(socket,nickNames,namesUsed);
+		handleRoomJoining(socket);
+		
+		socket.on('rooms',function(){
+			socket.emit('rooms',io.sockets.manager.rooms);
+		});
+		handleClientDisconnection(socket,nickNames,namesUsed);
+	});
+};
+
+function assignGuestName(socket,guestNumber,nickNames,namesUsed){
+	var name = 'Guest' + guestNumber;
+	nickNames[socket.id] = name;
+	socket.emit('nameResult',{
+		success:true,
+	name:name});
+	nameUsed.push(name);
+	return guestNumber + 1;
+}
+
+function joinRoom(socket,room){
+	socket.join(room);
+	currentRoom[socket.id] = room;
+	socket.emit('joinResult',{room:room});
+	socket.broadcast.to(room).emit('message',{
+	text:nickNames[socket.id] + ' has joined ' + room + '.'
+	});
+	
+	var usersInRoom = io.sockets.clients(room);
+	if(usersInRoom.length > 1){
+		var usersInRoomSummary = 'Users currently in ' + room + ': ';
+		for (var index in usersInRoom) {
+			var userSocketId = usersInRoom[index].id;
+			if(userSocketId != socket.id){
+				if(index > 0){
+					usersInRoomSummary += ', ';
+				}
+				usersInRoomSummary += nickNmaes[userSocketId];
+			}
+		}
+		usersInRoomSummary += '.';
+		socket.emit('message',{text:usersInRoomSummary});
+	}
+}
+
+function handleNameChangeAttempts(socket,nickNames,namesUsed){
+	socket.on('nameAttempt',function(name){
+		//名称是否符合规范
+		if(name.indexOf('Guest') == 0){
+			socket.emit('nameResult',{
+				success:false,
+				message:'Names cannot begin with "Guest"'
+			});
+		}else{
+		//名称是否已被占用
+			if(nameUsed.indexOf(name) == -1){
+				var previousName = nickNames[socket.id];
+				var previousNameIndex = namesUsed.indexOf(previousName);
+				nameUsed.push(name);
+				nickNames[socke.id] = name;
+				delete namesUsed[previousNameIndex];
+				socket.emit('nameResult',{
+					success:true,
+					name:name
+				});
+				socket.broadcast.to(currentRoom[socket.id]).emit('message',{
+					text:previousName + ' is now known as ' + name + '.'
+				});
+			}else{
+				socket.emit('nameResult',{
+					success:false,
+					message:'That name is already in use.'
+				});
+			}
+		}
+	});
+
+}
+
+function handleMessageBroadcasting(socket){
+	socket.on('message',function(message){
+		socket.broadcast.to(message.room).emit('message',{
+			text:nickNames[socket.id] + ': ' + message.text
+		});
+	});
+}
+
+function handleRoomJoining(socket){
+	socke.on('join',function(room){
+		socket.leave(currentRoom[socket.id]);
+		joinRoom(socket,room.newRoom);
+	});
+}
+
+function handleClientDisconnection(socket){
+	socket.on('disconnect',function(){
+		var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+		delete namesUsed[nameIndex];
+		delete nickNames[socket.id];
+	});
+}
+
+
